979fdb8a278ea76aae2e95875f641c0a075329f4
reimott <reimottgg@gmail.com>
2021-03-18 23:56:48 +0000

    init change

--- scripts/mathsAndWords.js
+++ scripts/mathsAndWords.js
@@ -7,7 +7,16 @@ var click_time = [];
 var time = 0;
 const timestep = 10;
 var error = 0;
-var words = ["world", "about", "again", "heart", "pizza", "water", "happy", "sixty"];
+var words = [
+  "world",
+  "about",
+  "again",
+  "heart",
+  "pizza",
+  "water",
+  "happy",
+  "sixty",
+];
 var recall = false;
 var reverseword = false;
 var init_click = true;
@@ -18,97 +27,96 @@ var reverse = memword.split("").reverse().join("");
 alert("Your word is: " + memword);
 
 for (j = 0; j < 2; j++) {
-    for (i = 0; i < 10; i++) {
-        values[j][i] = Math.floor(Math.random() * 30 + 1);
-
-    }
+  for (i = 0; i < 10; i++) {
+    values[j][i] = Math.floor(Math.random() * 30 + 1);
+  }
 }
 for (i = 0; i < 9; i++) {
-    results[i] = (values[0][i] - values[1][i]);
-    // document.write(results[i] + " ");
+  results[i] = values[0][i] - values[1][i];
+  // document.write(results[i] + " ");
 }
 function buttonClick() {
-    var anim = "fade-animation";
+  var anim = "fade-animation";
 
-    // change styling after started
-    if(init_click){
-        $('#myInput').removeAttr("placeholder");
-        $('#myBtn').text("SUBMIT");
-        init_click = false;
+  // change styling after started
+  if (init_click) {
+    $("#myInput").removeAttr("placeholder");
+    $("#myBtn").text("SUBMIT");
+    init_click = false;
+  }
+
+  // reapply anim styling if previously animated
+  if ($("#calc").hasClass(anim)) {
+    var el = $("#calc"),
+      newone = el.clone(true);
+    el.before(newone);
+    $("." + el.attr("class") + ":last").remove();
+  }
+
+  $("#calc").addClass("fade-animation");
+
+  click_time.push(time);
+  if (score < 10) {
+    input = parseInt(document.getElementById("myInput").value);
+    document.getElementById("myInput").value = "";
+    $("#calc").text(values[0][score] + "-" + values[1][score]);
+
+    if (results[score - 1] != input) {
+      errors += 1;
+    }
+  }
+  if (score == 10) {
+    document.getElementById("calc").innerHTML = "Please spell the new word";
+  }
+  if (score == 11) {
+    input = document.getElementById("myInput").value;
+    document.getElementById("myInput").value = "";
+    if (input === memword) {
+      recall = true;
+    } else {
+      end();
     }
 
-    // reapply anim styling if previously animated
-    if($('#calc').hasClass(anim)){
-        var el     = $('#calc'),  
-        newone = el.clone(true);   
-        el.before(newone);  
-        $("." + el.attr("class") + ":last").remove();
+    document.getElementById("calc").innerHTML =
+      "Please now type the word backwards";
+  }
+  if (score == 12) {
+    input = document.getElementById("myInput").value;
+    document.getElementById("myInput").value = "";
+    if (input === reverse) {
+      reverseword = true;
     }
-
-    $('#calc').addClass("fade-animation");
-
-    click_time.push(time)
-    if (score < 10) {
-        input = parseInt(document.getElementById('myInput').value)
-        document.getElementById('myInput').value = "";
-        $('#calc').text(values[0][score] + "-" + values[1][score]);
-        
-
-        if (results[score - 1] != input) {
-            errors += 1;
-        }
-    }
-    if (score == 10) {
-        document.getElementById('calc').innerHTML = "Please spell the new word";
-    }
-    if (score == 11) {
-        input = (document.getElementById('myInput').value)
-        document.getElementById('myInput').value = "";
-        if (input === memword) {
-            recall = true;
-        }
-        else {
-            end()
-        }
-
-        document.getElementById('calc').innerHTML = "Please now type the word backwards";
-    }
-    if (score == 12) {
-        input = (document.getElementById('myInput').value)
-        document.getElementById('myInput').value = "";
-        if (input === reverse) {
-            reverseword = true;
-        }
-        end()
-    }
-    score += 1;
+    end();
+  }
+  score += 1;
 }
 
 function update_time() {
-    time += timestep;
+  time += timestep;
 }
 function end() {
-    var t = 0;
-    t = (click_time[10] - click_time[0]
-    );
+  var t = 0;
+  t = click_time[10] - click_time[0];
 
-    alert("You made: " + errors + " errors and it took you " + t / 1000 + " seconds");
-    if (reverseword){
-        alert("You sucesfully recalled the word and reversed it");
-    }
-    if (!recall){
-        alert("You didnt manage to recall the word")
-    }
-    if (recall && !reverseword){
-        alert("You managed to recall the word but couldnt reverse it")
-    }
-
+  alert(
+    "You made: " + errors + " errors and it took you " + t / 1000 + " seconds"
+  );
+  if (reverseword) {
+    alert("You sucesfully recalled the word and reversed it");
+  }
+  if (!recall) {
+    alert("You didnt manage to recall the word");
+  }
+  if (recall && !reverseword) {
+    alert("You managed to recall the word but couldnt reverse it");
+  }
+  location.reload();
 }
 // Get the input field
 var input = document.getElementById("myInput");
 input.addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("myBtn").click();
-    }
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("myBtn").click();
+  }
 });
